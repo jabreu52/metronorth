d9ccf121a3946b5c1434c49dc7cc8a724beed2b6
Jamil Abreu <abreu.jamil@gmail.com>
2012-09-08 07:19:28 -0400

    commit

--- app/controllers/stop_times_controller.rb
+++ app/controllers/stop_times_controller.rb
@@ -1,10 +1,10 @@
 class StopTimesController < ApplicationController
   def index
-    sid = params[:starting_id]
-    did = params[:destination_id]
+    sid = params[:starting_id] if params[:starting_id]
+    did = params[:destination_id] if params[:destination_id]
     if sid.present? && did.present? && sid.to_i != did.to_i
       trips = Trip.all(stop_ids: [sid.to_i, did.to_i]).where(direction: sid.to_i < did.to_i ? 0 : 1)
-      @stop_times = StopTime.in(stop_id: sid.to_i, trip_id: trips.map(&:id)).gte(arrival_time: Time.now.to_i).asc(:arrival_time)
+      @stop_times = StopTime.in(stop_id: [sid.to_i], trip_id: trips.map(&:id).map(&:to_i)).gte(arrival_time: Time.now.to_i).asc(:arrival_time)
     else
       id = sid.present? ? sid.to_i : 1
       @stop_times = StopTime.where(stop_id: id).gte(arrival_time: Time.now.to_i).asc(:arrival_time)
