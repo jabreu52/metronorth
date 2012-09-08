6dfab9edd1d50ef76011906c7eb78fbcefba7318
Jamil Abreu <abreu.jamil@gmail.com>
2012-09-08 07:13:23 -0400

    commit

--- app/controllers/stop_times_controller.rb
+++ app/controllers/stop_times_controller.rb
@@ -2,11 +2,11 @@ class StopTimesController < ApplicationController
   def index
     sid = params[:starting_id]
     did = params[:destination_id]
-    if sid.present? && did.present? && sid != did
-      trips = Trip.all(stop_ids: [sid, did]).where(direction: sid.to_i < did.to_i ? 0 : 1)
-      @stop_times = StopTime.in(stop_id: sid, trip_id: trips.map(&:id)).gte(arrival_time: Time.now.to_i).asc(:arrival_time)
+    if sid.present? && did.present? && sid.to_i != did.to_i
+      trips = Trip.all(stop_ids: [sid.to_i, did.to_i]).where(direction: sid.to_i < did.to_i ? 0 : 1)
+      @stop_times = StopTime.in(stop_id: sid.to_i, trip_id: trips.map(&:id)).gte(arrival_time: Time.now.to_i).asc(:arrival_time)
     else
-      id = sid.present? ? sid : 1
+      id = sid.present? ? sid.to_i : 1
       @stop_times = StopTime.where(stop_id: id).gte(arrival_time: Time.now.to_i).asc(:arrival_time)
     end
     
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@ require 'csv'
 
 puts "Add Stop Times"
 CSV.foreach('db/data/stop_times.csv', {headers: true}) do |row|
-  stop_id = row[3].to_i
+  stop_id = row[3]
   arrival_time = Chronic.parse(row[1].length < 8 ? "0#{row[1]}" : row[1]).to_i
   departure_time = Chronic.parse(row[2].length < 8 ? "0#{row[2]}" : row[2]).to_i
   stop_sequence = row[4].to_i
   unless StopTime.where(stop_id: stop_id, arrival_time: arrival_time, stop_sequence: stop_sequence).present?  
     StopTime.create!(
       stop_id: stop_id,
-      trip_id: row[0].to_i,
+      trip_id: row[0],
       arrival_time: arrival_time,
       departure_time: departure_time,
       stop_sequence: stop_sequence
@@ -20,18 +20,18 @@ end
 puts "Add Stops"
 CSV.foreach('db/data/stops.csv', {headers: true}) do |row|
   Stop.create!(
-    stop_id: row[0].to_i,
+    stop_id: row[0],
     name: row[2],
-    trip_ids: StopTime.where(stop_id: row[0].to_i).map(&:trip_id)
+    trip_ids: StopTime.where(stop_id: row[0]).map(&:trip_id)
   )
 end
 
 puts "Add Trips"
 CSV.foreach('db/data/trips.csv', {headers: true}) do |row|
   Trip.create!(
-    trip_id: row[2].to_i,
+    trip_id: row[2],
     headsign: row[3],
-    direction: row[5].to_i,
+    direction: row[5],
     stop_ids: StopTime.where(trip_id: row[2].to_i).map(&:stop_id)
   )
 end
