8c8ba30dd58703c90e2833d710e22939103dc02a
Jamil Abreu <abreu.jamil@gmail.com>
2012-09-11 10:47:00 -0400

    commit

--- app/controllers/stop_times_controller.rb
+++ app/controllers/stop_times_controller.rb
@@ -2,6 +2,7 @@ class StopTimesController < ApplicationController
   def index
     sid = params[:starting_id] if params[:starting_id]
     did = params[:destination_id] if params[:destination_id]
+    current_time = (Time.now.hour * 60) + Time.now.min
     
     if Time.now.sunday?
       t = Trip.in(service_id: [2,3,6])
@@ -17,12 +18,13 @@ class StopTimesController < ApplicationController
     
     if sid.present? && did.present? && sid.to_i != did.to_i
       trips = t.all(stop_ids: [sid.to_i, did.to_i]).where(direction: sid.to_i < did.to_i ? 0 : 1)
-      @stop_times = StopTime.where(stop_id: sid.to_i).in(trip_id: trips.map(&:id)).gte(arrival_time: Time.now.to_i).asc(:arrival_time)
+      stop_times = StopTime.where(stop_id: sid.to_i).in(trip_id: trips.map(&:id))
     else
       id = sid.present? ? sid.to_i : 1
-      current_time = (Time.now.hour * 60) + Time.now.min
-      @stop_times = StopTime.where(stop_id: id).in(trip_id: t.map(&:id)).gte(arrival_time: current_time).asc(:arrival_time)
+      stop_times = StopTime.where(stop_id: id).in(trip_id: t.map(&:id))
     end
+    
+    @stop_times = stop_times.gte(arrival_time: current_time).asc(:arrival_time)
     
     respond_to do |format|
       format.js
